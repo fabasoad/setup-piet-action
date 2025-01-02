3f1c15007acf4007feba00e02708fcfca5a712ff
fabasoad <fabasoad@gmail.com>
2025-01-03 00:23:45 +0900

    Change slash

--- src/install-npiet.ps1
+++ src/install-npiet.ps1
@@ -9,4 +9,5 @@ New-Item -Path "$bin_dir" -ItemType Directory -Force | Out-Null
 Invoke-WebRequest -URI "http://www.bertnase.de/npiet/npiet-$version-win32.zip" -OutFile "$bin_dir\npiet.zip"
 Expand-Archive "$bin_dir\npiet.zip" -DestinationPath "$bin_dir"
 Remove-Item -Path "$bin_dir\npiet.zip"
+ls -la .
 echo "$bin_path" | Out-File -FilePath $Env:GITHUB_PATH -Encoding utf8 -Append

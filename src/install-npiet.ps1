c039933e74a10d274fcdb3d773a55c2b360ce538
fabasoad <fabasoad@gmail.com>
2025-01-03 00:18:59 +0900

    Move pwsh code to a separate file

---
+++ src/install-npiet.ps1
@@ -0,0 +1,12 @@
+param($input_version, $bin_dir, $bin_path)
+
+if ($input_version -eq "1.1") {
+  $version=$input_version
+} else {
+  $version=$input_version + "a"
+}
+New-Item -Path "$bin_dir" -ItemType Directory -Force | Out-Null
+Invoke-WebRequest -URI "http://www.bertnase.de/npiet/npiet-$version-win32.zip" -OutFile "$bin_dir\npiet.zip"
+Expand-Archive "$bin_dir\npiet.zip" -DestinationPath "$bin_dir"
+Remove-Item -Path "$bin_dir\npiet.zip"
+echo "$bin_path" | Out-File -FilePath $Env:GITHUB_PATH -Encoding utf8 -Append
